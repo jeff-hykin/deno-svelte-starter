53fd2322c64c13f458939cb0d7bf8981eb40cb6a
Jeff Hykin <jeff.hykin@gmail.com>
2025-08-28 21:31:00 -0500

    switch to svelte static

--- src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async ({ fetch }) => {
-  const res = await fetch(`/api/dinosaurs`);
-  const dinosaurs = await res.json();
-
-  return { dinosaurs };
-};
--- src/routes/api/dinosaurs/+server.js
+++
@@ -1,6 +0,0 @@
-import { json } from "@sveltejs/kit";
-import data from "../data.json" with { type: "json" };
-
-export function GET() {
-  return json(data);
-}
